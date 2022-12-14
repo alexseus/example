9148e80038f690a76ae276f1b686346b2432a792
aleksei <git@alexsei.ru>
2022-12-14 19:49:34 +0300

    example005

--- example003_sum/Program.cs
+++ example003_sum/Program.cs
@@ -1,4 +1,9 @@
-﻿int numberA = 31;
-int numberB = 4;
+﻿int numberA = new Random().Next(1,10);
+Console.Write("первое число: ");
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.Write("второе число: ");
+Console.WriteLine(numberB);
 int result = numberA + numberB;
+Console.Write("Сумма чисел равна: ");
 Console.WriteLine(result);
---
+++ example005_masha/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Enter your name: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "masha")
+{
+    Console.WriteLine("Hi! it's really MASHAAA!");
+}
+else
+{
+    Console.Write("Hello, ");
+    Console.WriteLine(username);
+}
